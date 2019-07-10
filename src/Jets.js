e0383e4c47d7a3b8b778df44627f257a8adfbdec
Cory House <housecor@gmail.com>
2019-07-10 10:41:01 -0400

    Move to state

--- src/Jets.js
+++ src/Jets.js
@@ -1,34 +1,48 @@
 import React from "react";
 
 class Jets extends React.Component {
-  renderJets(jet) {
-    return (
-      <tr key={jet.id}>
-        <td>{jet.make}</td>
-        <td>{jet.model}</td>
-      </tr>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jets: [
+        { id: 1, make: "Lockheed", model: "Stealth" },
+        { id: 2, make: "Lockheed", model: "F22" },
+        { id: 3, make: "Lockheed", model: "F35" }
+      ]
+    };
+
+    // binding in the constructor
+    // this.renderJets = this.renderJets.bind(this);
   }
 
-  render() {
-    const jets = [
-      { id: 1, make: "Lockheed", model: "Stealth" },
-      { id: 2, make: "Lockheed", model: "F22" },
-      { id: 3, make: "Lockheed", model: "F35" }
-    ];
+  deleteJet() {}
 
+  // Using concise arrow syntax
+  renderJets = jet => (
+    <tr key={jet.id}>
+      <td>
+        <button onClick={this.deleteJet}>Delete</button>
+      </td>
+      <td>{jet.make}</td>
+      <td>{jet.model}</td>
+    </tr>
+  );
+
+  render() {
     return (
       <>
         <h1>Jets</h1>
         <table>
           <thead>
             <tr>
+              <th />
               <th>Make</th>
               <th>Model</th>
             </tr>
           </thead>
           {/* Point free style (jets is automatically passed by JavaScript) */}
-          <tbody>{jets.map(this.renderJets)}</tbody>
+          <tbody>{this.state.jets.map(this.renderJets)}</tbody>
         </table>
       </>
     );
