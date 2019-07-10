cc40fc24c027bf6b43b31771ade300c5c5614825
Cory House <housecor@gmail.com>
2019-07-10 14:52:12 -0400

    convert to hooks

--- src/Jets.js
+++ src/Jets.js
@@ -1,59 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getJets } from "./api/jetsApi";
 
-class Jets extends React.Component {
-  constructor(props) {
-    super(props);
+function Jets(props) {
+  const [jets, setJets] = useState([]);
 
-    this.state = {
-      jets: []
-    };
+  useEffect(() => {
+    getJets().then(jets => setJets(jets));
+  }, []);
 
-    // binding in the constructor
-    // this.renderJets = this.renderJets.bind(this);
-  }
-
-  componentDidMount() {
-    getJets().then(jets => {
-      this.setState({ jets: jets });
-    });
-  }
-
-  deleteJet(id) {
+  function deleteJet(id) {
     // remove the Jet from the array of jets in state.
-    const jets = this.state.jets.filter(jet => jet.id !== id);
-    this.setState({ jets: jets });
+    setJets(jets.filter(jet => jet.id !== id));
   }
 
-  // Using concise arrow syntax
-  renderJets = jet => (
-    <tr key={jet.id}>
-      <td>
-        <button onClick={() => this.deleteJet(jet.id)}>Delete</button>
-      </td>
-      <td>{jet.make}</td>
-      <td>{jet.model}</td>
-    </tr>
-  );
-
-  render() {
+  function renderJets(jet) {
     return (
-      <>
-        <h1>Jets</h1>
-        <table>
-          <thead>
-            <tr>
-              <th />
-              <th>Make</th>
-              <th>Model</th>
-            </tr>
-          </thead>
-          {/* Point free style (jets is automatically passed by JavaScript) */}
-          <tbody>{this.state.jets.map(this.renderJets)}</tbody>
-        </table>
-      </>
+      <tr key={jet.id}>
+        <td>
+          <button onClick={() => deleteJet(jet.id)}>Delete</button>
+        </td>
+        <td>{jet.make}</td>
+        <td>{jet.model}</td>
+      </tr>
     );
   }
+
+  return (
+    <>
+      <h1>Jets</h1>
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>Make</th>
+            <th>Model</th>
+          </tr>
+        </thead>
+        {/* Point free style (jets is automatically passed by JavaScript) */}
+        <tbody>{jets.map(renderJets)}</tbody>
+      </table>
+    </>
+  );
 }
 
 export default Jets;
